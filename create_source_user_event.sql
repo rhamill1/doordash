9068d80e8440b0763f034d88d1f366acd3eef191
Ryan Hamill <ryanhamill@MesospheresMBP9.attlocal.net>
2020-07-15 16:15:40 -0700

    updates fields to varchar

--- create_source_user_event.sql
+++ create_source_user_event.sql
@@ -1,11 +1,12 @@
 
-create table source_user_event (
-    event_time text,
-    user_id text,
-    event_type text,
-    platform text,
-    country text,
-    region text,
-    device_id text,
-    initial_referring_domain text
+create table if not exists source_user_event (
+    event_time varchar(50),
+    user_id varchar(50),
+    event_type varchar(50),
+    platform varchar(50),
+    country varchar(50),
+    region varchar(50),
+    device_id varchar(50),
+    initial_referring_domain varchar(50)
 );
+
